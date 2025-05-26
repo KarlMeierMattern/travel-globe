c5d78308b1ea4b200d6152dd1b6511b1d4a94e9b
Karl-Alexander <99612323+KarlMeierMattern@users.noreply.github.com>
2025-05-26 22:10:14 +0200

    commit

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,13 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import "./index.css";
 import App from "./App.tsx";
-
-const queryClient = new QueryClient();
+import Providers from "../providers.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <QueryClientProvider client={queryClient}>
+    <Providers>
       <App />
-    </QueryClientProvider>
+    </Providers>
   </StrictMode>
 );
